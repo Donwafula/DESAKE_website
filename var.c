f8b5f9a7c3d828a9f8b178a7e1e5d72373418bba
Replit user <>
2023-10-01 09:56:45 +0000

    corrected betty

--- var.c
+++ var.c
@@ -5,27 +5,25 @@
   *@x: int
   *return: void
   */
-
-  
 void loop(int x)
 {
-  while (x < 20)
-    {
-      printf("%d", x);
-      printf("\n");
-      x++;
-    }
+	while (x < 20)
+	{
+		printf("%d", x);
+		printf("\n");
+		x++;
+	}
 }
 
 /**
   *main - function
   *return: 0
   */
-int main()
+int main(void)
 {
-  int x;
-
-  x = 0;
-  loop(x);
-  return (0);
+	int x;
+	
+	x = 0;
+	loop(x);
+	return (0);
 }
